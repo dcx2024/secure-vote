d81a624bfd4498239a2d3cd944a5ce49e379573f
ore <dcx2607@gmail.com>
2025-10-02 22:30:34 +0100

    Switched from browser router to hash router

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,28 +1,27 @@
-import React from 'react'
-import {Route, createBrowserRouter, createRoutesFromElements, RouterProvider} from 'react-router-dom'
-import VoterLogin from './components/VoterLogin'
-import CreatePoll from './components/CreatePoll'
-import VoterResult from './components/VoteResult'
-import AdminPolls from './components/AdminPolls'
-import AdminSignup from './components/AdminSignup'
-import AdminLogin from './components/AdminLogin'
-import VotePage from './components/VoterPage'
+import React from 'react';
+import { HashRouter, Routes, Route } from 'react-router-dom';
+import VoterLogin from './components/VoterLogin';
+import CreatePoll from './components/CreatePoll';
+import VoterResult from './components/VoteResult';
+import AdminPolls from './components/AdminPolls';
+import AdminSignup from './components/AdminSignup';
+import AdminLogin from './components/AdminLogin';
+import VotePage from './components/VoterPage';
 
 const App = () => {
+  return (
+    <HashRouter>
+      <Routes>
+        <Route path='/' element={<VoterLogin />} />
+        <Route path='/vote/:token' element={<VotePage />} />
+        <Route path='/admin/signup' element={<AdminSignup />} />
+        <Route path='/admin/login' element={<AdminLogin />} />
+        <Route path='/admin/create' element={<CreatePoll />} />
+        <Route path='/admin/poll' element={<AdminPolls />} />
+        <Route path='/result' element={<VoterResult />} />
+      </Routes>
+    </HashRouter>
+  );
+};
 
-  const router=createBrowserRouter( 
-    createRoutesFromElements(
-      <Route>
-      <Route path='/' index element={<VoterLogin/>} />
-      <Route path='/vote/:token' index element={<VotePage/>} />
-      <Route path='/admin/signup' index element={<AdminSignup/>} />
-      <Route path='/admin/login' index element={<AdminLogin/>}/>
-      <Route path='/admin/create' index element={<CreatePoll/>}/>
-      <Route path='/admin/poll' index element={<AdminPolls/>}/>
-      <Route path='/result' index element={<VoterResult/>} />
-      </Route>
-    )
-  )
-  return <RouterProvider router={router}/>
-}
-export default App
+export default App;
