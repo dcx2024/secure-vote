41da933905e9bd7b86ddc33f2629c2260530f632
ore <dcx2607@gmail.com>
2025-10-02 20:23:46 +0100

    The cors origin has been changed

--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,7 @@ app.use(express.json())
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 app.use(cookieParser())
 app.use(cors({
-    origin: 'http://localhost:5173',
+    origin: 'https://secure-vote-frontend-11q2.onrender.com/',
     credentials: true
 }))
 
